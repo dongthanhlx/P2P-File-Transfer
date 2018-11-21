1739f32ae40adcd2cc7680bee1d256e3a98af030
Dong Thanh <dongthanhlx@gmail.com>
2018-11-21 16:57:18 +0700

    save list file

--- server/server.cpp
+++ server/server.cpp
@@ -15,18 +15,7 @@
 #include<vector>
 using namespace std;
 
-
-// vector<char*> split(char listFile[]){
-//     char *index = strtok(listFile," ");
-//     vector<char*> temp;
-//     while(listFile){
-//         temp.push_back(index);
-//         index = strtok(NULL," ");
-//     }
-//     return temp;
-// }
-
-int main(){
+vector<char*> allFile;
     int server_sockfd, client_sockfd, nbytes, bufferSize = 1024;
     int server_len;
     struct sockaddr_in server_address;
@@ -35,6 +24,64 @@ int main(){
     int port = 1236, opt = 1;
     int max_client = 10;
 
+vector<char*> split(char listFile[]){
+    vector<char*> list;
+    char *temp = strtok(listFile, " ");
+    while(temp != NULL){
+        list.push_back(temp);
+        temp = strtok(NULL, " ");
+    }
+    return list;
+}
+
+class Peer{
+private:
+    char *IP;
+    int Port;
+    bool State = false;
+    vector<char*> ListFile;
+public:
+    void setIP(char *ip){
+        IP = ip;
+    }
+    char *getIP(){
+        return IP;
+    }
+
+    void setPort(int port){
+        Port = port;
+    }
+    int getPort(){
+        return Port;
+    }
+
+    void setState(bool state){
+        State = state;
+    }
+    bool getState(){
+        return State;
+    }
+
+    void setListFile(vector<char*> listFile){
+        ListFile = listFile;
+    }
+    vector<char*> getListFile(){
+        return ListFile;
+    }
+};
+
+vector<Peer> peer;
+
+bool checkFile(vector<char*> listFile, char *nameFile){
+    int i=0;
+    int size = listFile.size();
+    while(i<size){
+        if(strcmp(nameFile, listFile[i]) == 0)  return true;
+        i++;
+    }
+    return false;
+}
+void init(){
     bzero(&server_address, sizeof(server_address));
     server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
     memset(&server_address, '\0', sizeof(server_address));
@@ -47,7 +94,7 @@ int main(){
 
     if(bind(server_sockfd, (struct sockaddr*)&server_address, server_len) < 0){
         cout<<"Error in binding";
-        return 1;
+        return ;
     }
 
     //listen
@@ -55,6 +102,17 @@ int main(){
         perror("Listen Error");
     }
     cout<<"Server listen at port "<<port<<"\n";
+}
+void addFile(vector<char*> list){
+    int i=0;
+    int size = list.size();
+    while(i < size){
+        if(!checkFile(allFile, list[i]))    allFile.push_back(list[i]);
+    }
+}
+int main(){
+
+    init();
 
     while(1){
         unsigned int client_len = sizeof(client_address);
@@ -64,23 +122,29 @@ int main(){
             exit(1);
         }
 
+        Peer p ;
+        p.setIP(inet_ntoa(client_address.sin_addr));
+        p.setPort(ntohs(client_address.sin_port));
+        p.setState(true);
+        peer.push_back(p);
+
         while(1){
+                cout<<"..........";
                 cout<<"Server waiting...\n";
+                cout<<"..........";
                 char buffer[bufferSize];
                 char listFile[bufferSize];
-                int sizeList = 5;
                 vector<char*> list;
                 if((nbytes = read(client_sockfd, listFile,sizeof(listFile))) <= 0){
                     break;
                 }
-                cout<<"List file: "<<listFile<<"t\n";
-                char *temp = strtok(listFile, " ");
-                while(temp != NULL){
-                    list.push_back(temp);
-                    temp = strtok(NULL, " ");
-                }
-                for(int i=0; i<list.size(); i++){
-                    cout<<list[i]<<"\t";
+                list = split(listFile);
+                p.setListFile(list);
+                addFile(list);
+                int i=0;
+                int size = allFile.size();
+                while(i < size){
+                    cout<<allFile[i]<<"\t";
                 }
         }
         close(client_sockfd);
